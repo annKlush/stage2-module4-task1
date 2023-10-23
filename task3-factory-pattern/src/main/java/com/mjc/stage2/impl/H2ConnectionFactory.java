7630757c0f72de6afea6acbb7e65910a3f981f1b
annKlush <109134951+annKlush@users.noreply.github.com>
2023-10-23 16:02:44 +0300

    com

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -2,33 +2,32 @@ package com.mjc.stage2.impl;
 
 import com.mjc.stage2.ConnectionFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class H2ConnectionFactory implements ConnectionFactory {
-    private final String jdbcDriver;
-    private final String dbUrl;
-    private final String user;
-    private final String password;
-
-    public H2ConnectionFactory(Properties properties) {
-        jdbcDriver = properties.getProperty("jdbc_driver");
-        dbUrl = properties.getProperty("db_url");
-        user = properties.getProperty("user");
-        password = properties.getProperty("password");
-    }
-
     @Override
     public Connection createConnection() {
-        try {
-            Class.forName(jdbcDriver);
-            return DriverManager.getConnection(dbUrl, user, password);
-        } catch (ClassNotFoundException | SQLException e) {
+        Connection conn = null;
+        File propsFile = new File("src/main/resources/h2database.properties");
+
+        try (FileInputStream fis = new FileInputStream(propsFile.getAbsolutePath())){
+            Properties props = new Properties();
+            props.load(fis);
+
+            String url = props.getProperty("db_url");
+            String user = props.getProperty("user");
+            String password = props.getProperty("password");
+
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
             e.printStackTrace();
-            return null; // Handle the error as needed
         }
+
+        return conn;
     }
 }
 
